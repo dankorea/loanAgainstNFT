8b8ed746f97cfc471cd26302544d2c95e84ecb60
cc <xicheng412@gmail.com>
2022-11-23 17:18:19 +0800

    增加NFT列表展示样例

--- web/js/init.mjs
+++ web/js/init.mjs
@@ -29,3 +29,37 @@ window.onload = async () => {
     });
   });
 };
+
+function getItemListItem(item) {
+  const li = document.createElement("li");
+  li.className = "p-2 m-2 border";
+  const title = document.createElement("div");
+  title.innerText = item.title;
+  title.className = "text-xl text-bold font-serif";
+  li.appendChild(title);
+  const description = document.createElement("div");
+  description.innerText = item.description;
+  description.className = "text-sm";
+  li.appendChild(description);
+  const imgUrl = item.media[0].gateway;
+  if (imgUrl) {
+    const pic = new Image(100, 100);
+    pic.setAttribute("loading", "lazy");
+    pic.setAttribute("src", imgUrl);
+    li.appendChild(pic);
+  }
+  return li;
+}
+
+getNfts("0xF5FFF32CF83A1A614e15F25Ce55B0c0A6b5F8F2c").then((resp) => {
+  console.log(resp);
+  if (resp && resp.ownedNfts) {
+    const ul = document.createElement("ul");
+    ul.className = "grid grid-cols-3 gap-4"
+    document.getElementById("main").appendChild(ul);
+    resp.ownedNfts.forEach((item) => {
+      const li = getItemListItem(item);
+      ul.appendChild(li);
+    });
+  }
+});
